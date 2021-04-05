2e0089509cae3c6634a4aa572e55855d61d5a24e
yongri25 <yongri.liii@gmail.com>
2021-04-05 08:27:18 +0200

    put the footer on only the nps

---
+++ assets/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  var pathname = window.location.pathname;
+  if(pathname.includes('pages/nps')) {
+    var screen_height = $( window ).height();
+    var current_header_footer = $('.nav.clearfix').height() + $('.footer').height();
+    var nps_height = $('.container.main.content').height();
+    if(screen_height > (current_header_footer + nps_height)) {
+      $('.footer').addClass('footer-static');
+    }    
+  } else {
+    $('.footer').removeClass('footer-static');
+  }
+});
